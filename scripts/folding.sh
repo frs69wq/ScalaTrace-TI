e90f9f357a41e4178803281fcec87a98aea0fd63
suter <frederic.suter@cc.in2p3.fr>
2014-08-06 17:56:41 +0200

    end of analysis of the impact of folding

--- scripts/folding.sh
+++ scripts/folding.sh
@@ -2,9 +2,9 @@
 
 OUTPUT_DIR=../../../../../datasets 
 
-cd ../traces-new/ti/lu/C/folded
 
-########################## ACQUISITION TIMES #########################
+########################## TI ACQUISITION TIMES #########################
+cd ../traces-new/ti/lu/C/folded
 TIMES=$OUTPUT_DIR/ti_folding_times.csv 
 
 #Header
@@ -36,8 +36,34 @@ for i in {2,4,8,16,32}; do
     awk -F' ' '/real/{ printf ","$4","}' $i/details.I >> $TIMES
     echo $REGULAR >> $TIMES
 done
+cd -
 
-########################## TRACE CONTENTS  #########################
+########################## TD ACQUISITION TIMES #########################
+
+cd ../traces-new/td/lu/C/folded
+TIMES=$OUTPUT_DIR/td_folding_times.csv 
+#Header
+echo "Timer,Factor,cores,Time,Regular" > $TIMES
+
+# Application timer
+for c in {1,2,4}; do
+    awk -F'=' -v c=$c '/Time in/{gsub(" ","", $2 ); print "Application,1,"c","$2","$2}' $c-core*/1/details >> $TIMES
+    REGULAR=`awk -F'=' -v c=$c '/Time in/{gsub(" ","", $2 ); print $2}' $c-core*/1/details`
+    for i in {2,4,8,16,32}; do
+        if [ $c -eq 4 ] && [ $i -eq 32 ] 
+	then
+	    echo -n "Application,"$i","$c,"NA,NA" >> $TIMES;
+	else
+	    echo -n "Application,"$i","$c >> $TIMES;
+	    awk -F'=' '/Time in/{gsub(" ","", $2 ); printf ","$2","}' $c-core*/$i/details >> $TIMES;
+	    echo $REGULAR >> $TIMES;
+	fi
+    done
+done
+cd -
+
+########################## TI TRACE CONTENTS  #########################
+cd ../traces-new/ti/lu/C/folded
 #Header
 echo "eventID,opcode,subblock,COMP-4c.avg" > $OUTPUT_DIR/ti_folding_regular.csv
 
@@ -48,5 +74,15 @@ awk -F' ' '/EVENT/{gsub("EVENT|:","",$1); event= $1","$2}; /COMP/{printf event;g
 for i in {2,4,8,16,32}; do
     echo "eventID,opcode,subblock,COMP-4c.avg" > $OUTPUT_DIR/ti_folding_$i.csv
     awk -F' ' '/EVENT/{gsub("EVENT|:","",$1); event= $1","$2}; /COMP/{printf event;gsub("4c-|:","",$2); print","$2","$5}' $i/I/0 >> $OUTPUT_DIR/ti_folding_$i.csv
+done
+cd -
+
+########################## TD TRACE CONTENTS  #########################
+cd ../traces-new/td/lu/C/folded/1-core
+
+#Folded modes
+for i in {1,2,4,8,16,32}; do
+    echo "eventID,opcode,subblock,COMP-4b.avg" > ../$OUTPUT_DIR/td_folding_$i.csv
+    awk -F' ' '/EVENT/{gsub("EVENT|:","",$1); event= $1","$2}; /COMP/{printf event;gsub("4b-|:","",$2); print","$2","$5}' $i/0 >> ../$OUTPUT_DIR/td_folding_$i.csv
 done
 cd -
